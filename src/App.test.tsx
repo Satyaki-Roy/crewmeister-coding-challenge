b4a071e7b9fc2380219a1868b097198cfae7ad42
Satyaki Roy <satyaki.roy@Satyakis-MacBook-Pro-2.local>
2022-12-08 16:29:02 +0530

    Modifying the test script and also adding unit-test for App.tsx file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders learn react link', async() => {
+  const {container} = render(<App />);
+  await waitFor(() => {
+    expect(screen.getByText('Member Name')).toBeInTheDocument();
+  });
+  expect(container).toMatchSnapshot();
+
+  const linkElement = screen.getByText(/Absence Page/i);
   expect(linkElement).toBeInTheDocument();
 });
